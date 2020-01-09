0629275933a844e7507f6c702e4795a836d45d7c
Srinivas Palavarapu <spalavar@codeaurora.org>
2020-01-09 16:46:18 +0800

    CarService: Add AirplaneModeService for Car power management.
    AirplaneModeService is used for Wifi/BT power management
    
    Change-Id: Ia298ec40941f930440490807722eb90e5e6dfd41

--- service/src/com/android/car/ICarImpl.java
+++ service/src/com/android/car/ICarImpl.java
@@ -118,6 +118,7 @@ public class ICarImpl extends ICar.Stub {
     private final CarStatsService mCarStatsService;
     private final CarExperimentalFeatureServiceController mCarExperimentalFeatureServiceController;
     private final CarWatchdogService mCarWatchdogService;
+    private final AirplaneModeService mAirplaneModeService;
 
     private final CarServiceBase[] mAllServices;
 
@@ -256,6 +257,7 @@ public class ICarImpl extends ICar.Stub {
         } else {
             mCarWatchdogService = carWatchdogService;
         }
+        mAirplaneModeService = new AirplaneModeService(serviceContext);
 
         CarLocalServices.addService(CarPowerManagementService.class, mCarPowerManagementService);
         CarLocalServices.addService(CarPropertyService.class, mCarPropertyService);
@@ -305,6 +307,7 @@ public class ICarImpl extends ICar.Stub {
         allServices.add(mCarWatchdogService);
         // Always put mCarExperimentalFeatureServiceController in last.
         addServiceIfNonNull(allServices, mCarExperimentalFeatureServiceController);
+        allServices.add(mAirplaneModeService);
         mAllServices = allServices.toArray(new CarServiceBase[allServices.size()]);
     }
 
